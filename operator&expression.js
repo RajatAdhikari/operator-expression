706b1b9e8ba12781982bcc13ef950e2ac42966b8
RajatAdhikari <104211386+RajatAdhikari@users.noreply.github.com>
2022-06-06 15:17:39 +0530

    Add files via upload

---
+++ operator&expression.js
@@ -0,0 +1,56 @@
+const AddTwoNumbers = (a,b) => {
+    var result=a+b;
+    return result;
+  };
+  console.log("Question= 1")
+ const Is_Valid = (a,b) => {
+    if(a<10 && a>b){
+      return true;
+    }
+    else{
+      return false;
+    }
+  };
+  console.log("Question= 2")
+   const Check = (A, B) => 
+{
+    if((A%10==0 && B%10==0)||(A%10==0|| B%10==0)){
+      return true;
+    }
+    else{
+      return false;
+    }
+    
+};
+console.log("Question= 3")
+const First_Digit = (n) => {
+    return parseInt(n/1000);
+ };
+
+const Last_Digit = (n) => {
+    return n%10;
+ };
+ console.log("Question= 4")
+ const Find_the_remainder = (a,b) => {
+    let result =b%a;
+    return result;
+};
+console.log("Question= 5")
+const Multiply_two_number = (a,b) => {
+    let result=a*b;
+    return result;
+};
+console.log("Question= 6")
+const Sum = (A, B, C) => 
+{
+ let sum  = A+B+C
+ return sum;
+};
+console.log("Question= 7")
+const Average = (A, B, C) => 
+{
+ let total = (A,B,C);
+ let result = (total*100)/300;
+ return result;
+};
+console.log("Question= 8")
